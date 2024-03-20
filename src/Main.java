bfcf38ae02e025b37cebda466e860c5f02b933fd
tracer-compiler2022 <compilers@software.nju.edu.cb>
2024-03-20 09:51:18 +0000

    > make njusicol24049 root Linux lucky 5.15.0-100-generic #110-Ubuntu SMP Wed Feb 7 13:27:48 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  09:51:18 up  1:25,  3 users,  load average: 0.69, 0.77, 0.92 f53141dcdeab3fe5a775d929ddc3e03031803599

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main
 {    
     public static void main(String[] args){
-        System.out.println(args[0]);
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+        } catch (IOException e) {
+            System.err.println("error" + e.getMessage());
+        }
     }
 }
